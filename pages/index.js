d7977c4322c284aecc9aa4d16a0cfbb3b70ba29a
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-10-09 10:38:13 -0300

    modify the sentence

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu te amo. Vou te orgulhar muito ainda. Obrigado por todo apoio.</h1>;
+    return <h1>Eu te amo. Vou te orgulhar muito ainda. Obrigado por todo apoio!</h1>;
 }
 
 export default Home;
